2d6e7ebd3bfc6cfe2a4a2183707243d23589822a
tasnimassali <36279243+tasnimassali@users.noreply.github.com>
2021-09-06 14:50:17 +0100

    Count character occurrences jupyter notebook + script

---
+++ Application1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Enter your sentance
+str1 = input("Enter your sentance or word: ")
+#Enter the character
+str2 = input("Enter the character : ")
+#cout occurrences
+print(str1.count(str2))
+
+
+# In[ ]:
+
+
+
+
